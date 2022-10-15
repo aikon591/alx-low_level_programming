97f27b731fe1f2ea9f94ff97fddf1773d2bd34d4
aikon591 <aikonerik4goals@gmail.com>
2022-10-15 00:18:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,14 +17,21 @@ if (N > 0)
 {
 printf("%d is positive\n", n);
 }
+
 else if (n == 0)
+
 {
+
 else
+
 {
 printf("%d is zero\n", n);
 }
-else
+
+ else
+
 {
 printf("%d is negative\n", n);
 }
+
 return (0);
